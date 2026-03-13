6e04646f3c35a855446a029783aa46466bfb211e
rishi2882006-star <rishi2882006@gmail.com>
2026-03-13 19:46:19 +0530

    upload greeting website

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import time
+import random
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    greeting = ""
+    name = ""
+    thought = ""
+    bg = "day"
+
+    thoughts = [
+        "Your presence has a beautiful way of making every moment feel special.",
+        "Some people make the world brighter just by being in it, and you are one of them.",
+        "A beautiful day becomes even better when you are part of it.",
+        "Little moments become unforgettable with the right person around.",
+        "Your smile has a way of making everything feel lighter."
+    ]
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+
+        timestamp = int(time.strftime("%H"))
+
+        if 6 <= timestamp < 12:
+            greeting = "Good Morning"
+            bg = "morning"
+
+        elif 12 <= timestamp < 18:
+            greeting = "Good Afternoon"
+            bg = "afternoon"
+
+        elif 18 <= timestamp < 22:
+            greeting = "Good Evening"
+            bg = "evening"
+
+        else:
+            greeting = "Good Night"
+            bg = "night"
+
+        thought = random.choice(thoughts)
+
+    return render_template("index.html", greeting=greeting, name=name, thought=thought, bg=bg)
+
+
+app.run(debug=True)
